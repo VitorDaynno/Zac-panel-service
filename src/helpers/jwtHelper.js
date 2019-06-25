115b97c227e1edc40aacdf4f4209d4c0464d29c7
VitorDaynno <vitordaynno@gmail.com>
2019-06-25 19:08:20 -0300

    Melhoria na leitura de data de expiração do JWT

--- src/helpers/jwtHelper.js
+++ src/helpers/jwtHelper.js
@@ -6,7 +6,7 @@ class JWTHelper {
         const jwtToken = jwt.sign(
             this.entity,
             process.env.JWT_SECRET,
-            { expiresIn: process.env.JWT_EXPIRES_IN },
+            { expiresIn: process.env.JWT_EXPIRES_IN * 60 },
         );
         return jwtToken;
     }
